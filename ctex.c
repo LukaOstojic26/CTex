532b00312f77fec8a70d6d642816f7f305e6f50e
LukaOstojic26 <lukalbj26@gmail.com>
2025-02-25 16:59:33 +0100

    dodaci

--- ctex.c
+++ ctex.c
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -14,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -39,6 +46,51 @@ void enableRawMode() {
 	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+	int nread;
+	char c;
+
+	while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if(nread == -1 && errno != EAGAIN) die("read");
+	}
+
+	return c;
+}
+
+/*** output ***/
+
+void editorDrawRows() {
+	int y;
+	for(y = 0; y < 24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void editorRefreshScreen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+	char c = editorReadKey();
+
+	switch(c) {
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0);
+			break;
+	}
+
+}
+
 /*** init ***/
 
 int main() {
@@ -47,19 +99,9 @@ int main() {
 
 	while(1) {
 
-		char c = '\0';
-		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+		editorRefreshScreen();
+		editorProcessKeypress();	
 
-		if(iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d (%c)\r\n", c, c);
-		}
-
-		if(c == 'q'){
-			break;
-		}
-		
 	}
 		
 
